3ee548614eb8ebe375edbab6dc9a35e15c8254f5
flpsno <flpsno@hotmail.com>
2017-03-26 10:29:46 -0300

    Alterado a base url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://www.flpsno.com.br/dipapel/index.php/';
-$config['base_url'] = 'http://localhost/dipapel/';
+$config['base_url'] = 'http://localhost/dipapelweb/';
 
 /*
 |--------------------------------------------------------------------------
